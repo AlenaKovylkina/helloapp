610c6f39696f382bdc535b395549667b9b6ca643
white <white@example.com>
2026-07-09 17:09:14 +0500

    Add greetWhite method

--- helloapp-black/Program.cs
+++ helloapp-black/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("hello");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("hello");
+    }
+
+    static void greetWhite()
+    {
+        Console.WriteLine("hello white");
+    }
+}
